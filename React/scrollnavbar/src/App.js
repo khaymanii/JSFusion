1b62beab7d627f3593252b1fea3e58c61bf8f180
khaymanii <kennethakpo61@gmail.com>
2024-03-04 16:16:25 +0100

    feat: created scrollnavbar project for react

---
+++ React/scrollnavbar/src/App.js
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      setIsScrolled(scrollTop > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <h1>Logo</h1>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default App;
