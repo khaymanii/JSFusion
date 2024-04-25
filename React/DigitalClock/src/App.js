cf276fa9f505c4fddac75d5f608c77c627fdfa32
syedmaazsaeed <syedmaazsaeed@gmail.com>
2024-04-26 00:21:32 +0500

    Adding Code

---
+++ React/DigitalClock/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(new Date());
+  const [is12HourFormat, setIs12HourFormat] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const toggleFormat = () => {
+    setIs12HourFormat((prevFormat) => !prevFormat);
+  };
+
+  const formatTime = (time) => {
+    if (is12HourFormat) {
+      return time.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+      });
+    } else {
+      return time.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      });
+    }
+  };
+
+  const formatDate = (time) => {
+    return time.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
+  const formattedTime = formatTime(time);
+  const formattedDate = formatDate(time);
+
+  return (
+    <div className="App">
+      <h1>Digital Clock</h1>
+      <div className="time">{formattedTime}</div>
+      <div className="date">{formattedDate}</div>
+      <button onClick={toggleFormat}>
+        Toggle {is12HourFormat ? '24-hour' : '12-hour'} Format
+      </button>
+    </div>
+  );
+}
+
+export default App;
